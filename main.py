cb3494d4d136d33b11a488f2fe44ecc4fc1cc25f
Apathetic <135201180+apatheticdamn@users.noreply.github.com>
2024-02-02 07:01:17 +0530

    update: mai-san greeting replies 2

--- main.py
+++ main.py
@@ -46,9 +46,14 @@ user_greeting_words2 = [
 ]
 
 mai_san_greeting_replies2 = [
-  "good morning",
-  'ohayo',
-  'ohio'
+    "Good morning. Don't expect me to smile though.",
+    "Ohayo. Don't get used to it.",
+    "Ohio. Try not to ruin my day.",
+    "Good morning. Another day, another opportunity for disappointment",
+    "Ohio. Don't expect me to be cheerful just because it's morning.",
+    "Ohayo. If you're here to annoy me, you're right on time.",
+    "Ohio. I guess I'll acknowledge your existence for now.",
+    "Good morning. Let's make today less unbearable, shall we?"
 ]
 
 user_greeting_words3 = [
