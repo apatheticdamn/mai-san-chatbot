38d214ddf92ed4b414bdce80c468e4a662043806
Apathetic <135201180+apatheticdamn@users.noreply.github.com>
2024-02-02 07:03:39 +0530

    update: mai-san greeting replies 4

--- main.py
+++ main.py
@@ -91,7 +91,14 @@ user_greeting_words4 = [
 
 mai_san_greeting_replies4 = [
   "good evening",
-  'Konbanwa',
+  "Konbanwa. Try not to bore me with pleasantries.",
+    "Good evening. Let's see if you can make this conversation mildly interesting.",
+    "Konbanwa. Don't expect me to be cheerful just because it's evening.",
+    "Good evening. Is there a reason you're still here?",
+    "Konbanwa. I hope you have something worthwhile to say.",
+    "Konbanwa. Don't expect me to be impressed by your timing.",
+    "Good evening. I'll try not to fall asleep during our conversation.",
+    "Konbanwa. Let's see if you can entertain me for once."
 ]
 
 user_ask_currect_state = [
